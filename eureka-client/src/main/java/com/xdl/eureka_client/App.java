265ede595f4b3d997c54a03e0fdef35da1849d01
daixilong <1113024640@qq.com>
2018-03-31 17:26:13 +0800

    20180331

---
+++ eureka-client/src/main/java/com/xdl/eureka_client/App.java
@@ -0,0 +1,21 @@
+package com.xdl.eureka_client;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	new SpringApplicationBuilder(
+          App.class)
+          .web(true).run(args);
+    }
+}
